729b192017c07d782a83f18089067916c601305d
abhsihekc063 <abhishekc063@gmail.com>
2020-10-09 14:49:00 +0530

    project continuation

---
+++ project-2/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingridient } from '../shared/ingridient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+
+  ingridients:Ingridient[] =[
+    new Ingridient('Tomato',5),
+    new Ingridient('Apple',10)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onIngridientAdded(newIngredient:Ingridient){
+    this.ingridients.push(newIngredient);
+  }
+
+}
